1f7695568a36e916ace568b13d59843eb9a72b9e
hetalhalani <66283107+hetalhalani@users.noreply.github.com>
2022-11-18 16:05:39 -0800

    Update CanSat-code/CanSat-payload/lib/MiniSpyCamera.hpp
    Co-authored-by: Adrien <50222631+acarrou@users.noreply.github.com>

--- CanSat-code/CanSat-payload/lib/MiniSpyCamera.hpp
+++ CanSat-code/CanSat-payload/lib/MiniSpyCamera.hpp
@@ -7,6 +7,11 @@ namespace CanSat
         /*Connect the red wire to 3.7V-5VDC and the black wire to ground. Then use the white wire to control the camera. When touched to ground for under about 100ms, the camera will take a photo and stick it in a PHOTO folder on the SD card, numbered from PIC000.jpg up to PIC999.jpg.*/
         private:
            int trig;
+            public:
+            MiniSpyCamera(int &trig_pin)
+            {
+               trig = trig_pin
+            }
             
 
         void setup() {                
