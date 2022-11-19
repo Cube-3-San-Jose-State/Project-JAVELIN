58cbd6c0a720d98c48fa23f31fd785af7a2f3a16
Ted Qiao <ted4523@Teds-MacBook-Pro.local>
2022-11-18 17:45:42 -0800

    testing

--- CanSat-code/CanSat-container/src/main.cpp
+++ CanSat-code/CanSat-container/src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 #include "../lib/MAX17048.hpp"
+Adafruit_MAX17048 fuelguage;
 void setup() {
-
+  Serial.begin(9600);
+  while (!Serial) delay (10);
+  fuelguage.quickStart();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print(fuelguage.cellPercent());
+  Serial.print(fuelguage.cellVoltage());
 }
